c17998bf886b016cc63317be0106e49439a36fea
Iryna Yelunina <lucky.asterisk@gmail.com>
2023-11-23 20:14:45 +0100

    update

--- src/ait/map/WordFreqAppl.java
+++ src/ait/map/WordFreqAppl.java
@@ -2,6 +2,12 @@ package ait.map;
 
 import java.util.*;
 
+/*
+abc - 1
+ab - 3
+limn - 2
+a - 1
+ */
 public class WordFreqAppl {
     public static void main(String[] args) {
         String[] words = {"abc", "ab", "limn", "limn", "ab", "a", "ab"};
@@ -10,22 +16,27 @@ public class WordFreqAppl {
 
     private static void printWordsFrequency(String[] words) {
         Map<String, Integer> res = new HashMap<>();
-        for (int i = 0; i < words.length; i++) {
-//            if (res.containsKey(words[i])) {
-//                res.put(words[i], res.get(words[i]) + 1);
+        for (String word : words) {
+//            if (!res.containsKey(word)) {
+//                res.put(word, 1);
 //            } else {
-//                res.put(words[i], 1);
+//                res.put(word, res.get(word) + 1);
 //            }
 
-//            if (res.putIfAbsent(words[i], 1) != null) {
-//                res.put(words[i], res.get(words[i]) + 1);
+//            if (res.putIfAbsent(word, 1) != null) {
+//                res.put(word, res.get(word) + 1);
 //            }
 
-//            res.computeIfPresent(words[i], (k, v) ->  v + 1);
-//            res.computeIfAbsent(words[i], k -> 1);
+//            Integer frequency = res.getOrDefault(word, 0);
+//            res.put(word, frequency + 1);
 
-            res.merge(words[i], 1, (v1, v2) -> v1 + v2);
+//            res.computeIfPresent(word, (k, v) -> v + 1);
+//            res.computeIfAbsent(word, k -> 1);
+
+//            res.merge(word, 10, (v1, v2) -> v1 * v2 );
+            res.merge(word, 1, (v1, v2) -> v1 + v2 );
         }
+//        System.out.println(res);
         Set<Map.Entry<String, Integer>> entries = res.entrySet();
         System.out.println("===== Unsorted =====");
         for (Map.Entry<String, Integer> entry : entries) {
@@ -34,6 +45,6 @@ public class WordFreqAppl {
         System.out.println("===== Sorted by value desc =====");
         List<Map.Entry<String, Integer>> list = new ArrayList<>(entries);
         list.sort((e1, e2) -> Integer.compare(e2.getValue(), e1.getValue()));
-        list.forEach(e -> System.out.println(e.getKey() + " -> " + e.getValue()));
+        list.forEach(e -> System.out.println(e));
     }
 }
